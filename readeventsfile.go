aa34b8ceaba9d29f1639b6140cdc45a69741f078
jgough <joe.gough@datatrails.ai>
2024-07-03 15:30:26 +0100

    use logverification event structures
    re: AB#9604

--- readeventsfile.go
+++ readeventsfile.go
@@ -6,18 +6,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/datatrails/go-datatrails-logverification/logverification"
 	"github.com/urfave/cli/v2"
 )
 
 // readArgs0File assumes the first program argument is a file name and reads it
-func readArgs0File(cCtx *cli.Context) ([]VerifiableEvent, error) {
+func readArgs0File(cCtx *cli.Context) ([]logverification.VerifiableEvent, error) {
 	if cCtx.Args().Len() < 1 {
 		return nil, fmt.Errorf("filename expected as first positional command argument")
 	}
 	return ReadVerifiableEventsFromFile(cCtx.Args().Get(0))
 }
 
-func readArgs0FileOrStdIo(cCtx *cli.Context) ([]VerifiableEvent, error) {
+func readArgs0FileOrStdIoToVerifiableEvent(cCtx *cli.Context) ([]logverification.VerifiableEvent, error) {
 	if cCtx.Args().Len() > 0 {
 		return ReadVerifiableEventsFromFile(cCtx.Args().Get(0))
 	}
@@ -32,12 +33,27 @@ func readArgs0FileOrStdIo(cCtx *cli.Context) ([]VerifiableEvent, error) {
 	return VerifiableEventsFromData(data)
 }
 
+func readArgs0FileOrStdIoToDecodedEvent(cCtx *cli.Context) ([]logverification.DecodedEvent, error) {
+	if cCtx.Args().Len() > 0 {
+		return ReadDecodedEventsFromFile(cCtx.Args().Get(0))
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	var data []byte
+	for scanner.Scan() {
+		data = append(data, scanner.Bytes()...)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return DecodedEventsFromData(data)
+}
+
 // ReadVerifiableEventsFromFile reads datatrails events from a file and returns a
 // normalized list of raw binary items.
 //
 // See EventListFromData for the content expectations (must be a list of events
 // or single event from datatrails api)
-func ReadVerifiableEventsFromFile(fileName string) ([]VerifiableEvent, error) {
+func ReadVerifiableEventsFromFile(fileName string) ([]logverification.VerifiableEvent, error) {
 
 	data, err := os.ReadFile(fileName)
 	if err != nil {
@@ -46,7 +62,7 @@ func ReadVerifiableEventsFromFile(fileName string) ([]VerifiableEvent, error) {
 	return VerifiableEventsFromData(data)
 }
 
-func VerifiableEventsFromData(data []byte) ([]VerifiableEvent, error) {
+func VerifiableEventsFromData(data []byte) ([]logverification.VerifiableEvent, error) {
 
 	// Accept either the list events response format or a single event. Peak
 	// into the json data to pick which.
@@ -55,15 +71,43 @@ func VerifiableEventsFromData(data []byte) ([]VerifiableEvent, error) {
 		return nil, err
 	}
 
-	var verifiableEvents []VerifiableEvent
-	for _, raw := range eventsJson {
-		ve, err := NewVerifiableEvent([]byte(raw))
-		if err != nil {
-			return nil, err
-		}
-		verifiableEvents = append(verifiableEvents, ve)
+	verifiableEvents, err := logverification.NewVerifiableEvents(eventsJson)
+	if err != nil {
+		return nil, err
 	}
+
 	return verifiableEvents, nil
+}
+
+// ReadDecodedEventsFromFile reads datatrails events from a file and returns a
+// normalized list of raw binary items.
+//
+// See EventListFromData for the content expectations (must be a list of events
+// or single event from datatrails api)
+func ReadDecodedEventsFromFile(fileName string) ([]logverification.DecodedEvent, error) {
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return DecodedEventsFromData(data)
+}
+
+func DecodedEventsFromData(data []byte) ([]logverification.DecodedEvent, error) {
+
+	// Accept either the list events response format or a single event. Peak
+	// into the json data to pick which.
+	eventsJson, err := EventListFromData(data)
+	if err != nil {
+		return nil, err
+	}
+
+	decodedEvents, err := logverification.NewDecodedEvents(eventsJson)
+	if err != nil {
+		return nil, err
+	}
+
+	return decodedEvents, nil
 }
 
 // EventListFromData normalises a json encoded event or *list* of events, by
@@ -76,7 +120,7 @@ func VerifiableEventsFromData(data []byte) ([]VerifiableEvent, error) {
 //
 //		{ events: [{event-0}, {event-1}, ..., {event-n}] }
 //	 Or just {event}
-func EventListFromData(data []byte) ([]json.RawMessage, error) {
+func EventListFromData(data []byte) ([]byte, error) {
 	var err error
 
 	doc := struct {
@@ -86,8 +130,28 @@ func EventListFromData(data []byte) ([]json.RawMessage, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// if we have a list event api response, use that
 	if len(doc.Events) > 0 {
-		return doc.Events, nil
+		return data, nil
 	}
-	return []json.RawMessage{json.RawMessage(data)}, nil
+
+	// otherwise we will create a list event api response based on the point get
+	// event api response.
+
+	var event json.RawMessage
+
+	err = json.Unmarshal(data, &event)
+	if err != nil {
+		return nil, err
+	}
+
+	doc.Events = append(doc.Events, event)
+
+	events, err := json.Marshal(&doc)
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
 }

dc0326b028a635229df42b2220fe8416d05cdcfa
Robin Bryce <robin.bryce@datatrails.ai>
2024-08-28 18:06:16 +0100

    test: add test for single massif horizoned replicaiton

--- tests/verifyconsistency/verifyconsistency_azurite_test.go
+++ tests/verifyconsistency/verifyconsistency_azurite_test.go
@@ -53,6 +53,54 @@ func (s *VerifyConsistencyCmdSuite) Test4AzuriteMassifsForOneTenant() {
 	}
 }
 
+func (s *VerifyConsistencyCmdSuite) Test4AzuriteSingleAncestorMassifsForOneTenant() {
+
+	logger.New("TestLocalMassifReaderGetVerifiedContext")
+	defer logger.OnExit()
+
+	tc := massifs.NewLocalMassifReaderTestContext(
+		s.T(), logger.Sugar, "TestLocalMassifReaderGetVerifiedContext")
+
+	massifCount := uint32(4)
+	massifHeight := uint8(8)
+
+	tenantId0 := tc.G.NewTenantIdentity()
+	tc.CreateLog(tenantId0, massifHeight, uint64(massifCount*(1<<massifHeight)+0))
+
+	replicaDir := s.T().TempDir()
+
+	// note: VERACITY_IKWID is set in main, we need it to enable --envauth so we force it here
+	app := veracity.NewApp(true)
+	veracity.AddCommands(app, true)
+
+	err := app.Run([]string{
+		"veracity",
+		"--envauth", // uses the emulator
+		"--container", tc.TestConfig.Container,
+		"--data-url", s.Env.AzuriteVerifiableDataURL,
+		"--tenant", tenantId0,
+		"--height", fmt.Sprintf("%d", massifHeight),
+		"verify-consistency",
+		"--ancestors", "1",
+		"--replicadir", replicaDir,
+		"--massif", fmt.Sprintf("%d", massifCount-1),
+	})
+	s.NoError(err)
+
+	// check the 0'th massifs and seals were _not_ replicated
+	expectMassifFile := filepath.Join(replicaDir, massifs.ReplicaRelativeMassifPath(tenantId0, 0))
+	s.NoFileExistsf(expectMassifFile, "the replicated massif should NOT exist")
+	expectSealFile := filepath.Join(replicaDir, massifs.ReplicaRelativeSealPath(tenantId0, 0))
+	s.NoFileExistsf(expectSealFile, "the replicated seal should NOT exist")
+
+	for i := uint32(2); i < massifCount; i++ {
+		expectMassifFile := filepath.Join(replicaDir, massifs.ReplicaRelativeMassifPath(tenantId0, i))
+		s.FileExistsf(expectMassifFile, "the replicated massif should exist")
+		expectSealFile := filepath.Join(replicaDir, massifs.ReplicaRelativeSealPath(tenantId0, i))
+		s.FileExistsf(expectSealFile, "the replicated seal should exist")
+	}
+}
+
 func (s *VerifyConsistencyCmdSuite) Test4AzuriteMassifsForThreeTenants() {
 
 	logger.New("TestLocalMassifReaderGetVerifiedContext")

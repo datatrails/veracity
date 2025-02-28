b897dc985250b497d277dacbc42f6554c5324726
Robin Bryce <robin.bryce@datatrails.ai>
2025-02-28 12:42:08 +0000

    tests: add tests for range panics due to log/seal mismatch
    AB#10433

--- tests/systemtest/test.sh
+++ tests/systemtest/test.sh
@@ -80,25 +80,27 @@ testVeracityWatchLatestFindsActivity() {
 
 testVeracityReplicateLogsPublicTenantWatchPipe() {
     local output
+    local replicadir=$TEST_TMPDIR/merklelogs
 
-    rm -rf $TEST_TMPDIR/merkelogs
+    rm -rf $replicadir
     output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata \
         --tenant=$PROD_PUBLIC_TENANT_ID watch --horizon 10000h \
-        | $VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --ancestors=0 --replicadir=$TEST_TMPDIR/merkelogs)
+        | $VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --ancestors=0 --replicadir=$replicadir)
     assertEquals "watch-public should return a 0 exit code" 0 $?
 
-    COUNT=$(find $TEST_TMPDIR/merkelogs -type f | wc -l | tr -d ' ')
+    COUNT=$(find $replicadir -type f | wc -l | tr -d ' ')
     assertEquals "should replicate one massif and one seal" "2" "$COUNT"
 }
 
 testVeracityReplicateLogsPublicTenantWatchLatestFlag() {
     local output
+    local replicadir=$TEST_TMPDIR/merklelogs
 
-    rm -rf $TEST_TMPDIR/merkelogs
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --latest --ancestors=0 --replicadir=$TEST_TMPDIR/merkelogs)
+    rm -rf $replicadir
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --latest --ancestors=0 --replicadir=$replicadir)
     assertEquals "replicate-logs --latest should return a 0 exit code" 0 $?
 
-    COUNT=$(find $TEST_TMPDIR/merkelogs -type f | wc -l | tr -d ' ')
+    COUNT=$(find $replicadir -type f | wc -l | tr -d ' ')
     assertEquals "should replicate one massif and one seal" "2" "$COUNT"
 }
 
@@ -154,66 +156,80 @@ testFindMMREntrySingleEventWithLocalMassifCopy() {
     assertEquals "verifying finding the mmr entry of a known public prod event from a local log should match mmr index 663" "matches: [663]" "$output"
 }
 
-# test veracity can't update the replica for a tenant whos log has been tampered with
-#
-# in this case the tampering is copying a different tenant's log and eventually seal and overriding the originals.
-# this specific test, the overridden massif was partially full, and its overridden with a full massif.
-testReplicateLocalErrorForTamperedPartialToFullMassif() {
+testReplicateErrorForLogShorterThanSeal() {
 
     local output
+    local other_tenant
+    local tampered_log_url
+    local tampered_seal_url
+    local replicadir=$TEST_TMPDIR/merklelogs
 
+    # Note: this tenant belongs to Joe Gough and he has promised never to fill the first massif
     other_tenant=tenant/97e90a09-8c56-40df-a4de-42fde462ef6f
 
-    # first get the prod tenant replicated for massif 0. NOTE: this is a partially full massif
-    rm -rf $TEST_TMPDIR/merkelogs
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata \
-        --tenant=$other_tenant watch --horizon 10000h \
-        | $VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$other_tenant replicate-logs --ancestors=0 --replicadir=$TEST_TMPDIR/merkelogs)
-    assertEquals "watch-public should return a 0 exit code" 0 $?
+    rm -rf $replicadir
+    # first get the prod public tenant replicated for massif 0. NOTE: this is a full massif
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --massif 0 --replicadir=$replicadir)
+    assertEquals "0: should return a 0 exit code" 0 $?
 
-    COUNT=$(find $TEST_TMPDIR/merkelogs -type f | wc -l | tr -d ' ')
+    COUNT=$(find $replicadir -type f | wc -l | tr -d ' ')
     assertEquals "should replicate one massif and one seal" "2" "$COUNT"
 
-    # now get a different prod public tenant log and seal. NOTE: this is a full massif
-    tampered_log_url=${tampered_log_url:-${DATATRAILS_URL}/verifiabledata/merklelogs/v1/mmrs/${PROD_PUBLIC_TENANT_ID}/0/massifs/0000000000000000.log}
-    tampered_seal_url=${tampered_seal_url:-${DATATRAILS_URL}/verifiabledata/merklelogs/v1/mmrs/${PROD_PUBLIC_TENANT_ID}/0/massifseals/0000000000000000.sth}
+    # now get a different prod tenant log and seal. NOTE the log is partially full for this tenant
+    tampered_log_url=${tampered_log_url:-${DATATRAILS_URL}/verifiabledata/merklelogs/v1/mmrs/${other_tenant}/0/massifs/0000000000000000.log}
+    tampered_seal_url=${tampered_seal_url:-${DATATRAILS_URL}/verifiabledata/merklelogs/v1/mmrs/${other_tenant}/0/massifseals/0000000000000000.sth}
     curl -s -H "x-ms-blob-type: BlockBlob" -H "x-ms-version: 2019-12-12" $tampered_log_url -o tampered.log
-    curl -s -H "x-ms-blob-type: BlockBlob" -H "x-ms-version: 2019-12-12" $tampered_seal_url -o tampered.sth
 
-    # copy over the different tenant log for massif 0
-    cp tampered.log $TEST_TMPDIR/merkelogs/$other_tenant/0/massifs/0000000000000000.log
+    # copy over the different (shorter) tenant log for massif 0
+    cp tampered.log $replicadir/$PROD_PUBLIC_TENANT_ID/0/massifs/0000000000000000.log
 
-    # attempt to replicate the logs again
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$other_tenant replicate-logs --latest --replicadir=$TEST_TMPDIR/merkelogs)
-    assertEquals "a tampered log should exit 1" 1 $?
-    assertContains "$output"  "error: the seal signature verification failed: failed to verify seal for massif 0 for tenant tenant/97e90a09-8c56-40df-a4de-42fde462ef6f: verification error"
-
-    # now attempt to change the seal to the tampered log seal
-    cp tampered.sth $TEST_TMPDIR/merkelogs/$other_tenant/0/massifseals/0000000000000000.sth
-
-    # attempt to replicate the logs again
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$other_tenant replicate-logs --latest --replicadir=$TEST_TMPDIR/merkelogs)
-    assertEquals "a tampered log should exit 1" 1 $?
-    assertContains "$output"  "error: index out of range"
-
+    # attempt to replicate the logs again, the local log data is for the wrong
+    # tenant and is *less* than the seal expects, but the local seal is correct
+    # for the replaced data and the remote seal
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --replicadir=$replicadir)
+    assertEquals "1: a tampered log should exit 1" 1 $?
+    assertContains "$output"  "error: There is insufficient data in the massif context to generate a consistency proof against the provided state"
 }
 
-# test veracity can't update the replica for a tenant whos log has been tampered with
+# test veracity can't extend the replica of the wrong tenant
 #
-# in this case the tampering is copying a different tenant's log and eventually seal and overriding the originals.
-# this specific test, the overridden massif was full, and its overridden with a partial massif.
-testReplicateLocalErrorForTamperedFullToPartialMassif() {
+# When extending a local replica, if the local tenant log data is from a tenant
+# other than the requested remote, replication should fail due to consistency
+# checks. This is essentially equivalent to a tamper attempt.
+#
+# There are two cases that are important:
+#   1. The higest indexed local massif is incomplete and so the remote massif is used to extend it.
+#   2. The highest indexed local massif is complete and so the remote massif is copied, leaving the original unchanged.
+#
+# This test suite covers only the second case. Thee first case can only be
+# tested with synthesized data, or interaction with a live system, and so is
+# easier to cover in the integration tests. However, the same checks are
+# excercised in both cases and so this test gives a lot of confidence both
+# situations are sound.
+#
+# Note that the --ancestor flag can be used to limit how many massifs are
+# replicated. This can cause the replica to "start again" because the replica is
+# so far behind that the --ancestor limit forces a gap. In this case consistency
+# of the remote is not checked against the local massif, and in that case the
+# replication would succeded, the local replica of the foregn tenant would not
+# be updated. And the replica would be left with massifs from multiple tenants.
+testReplicateErrorForMixedTenants() {
 
     local output
+    local other_tenant
+    local tampered_log_url
+    local tampered_seal_url
+    local replicadir=$TEST_TMPDIR/merklelogs
 
-    other_tenant=tenant/97e90a09-8c56-40df-a4de-42fde462ef6f
+    # Note: this tenant is known to have > 1 massif at the time of writing and logs don't get shorter
+    other_tenant=tenant/b197ba3c-44fe-4b1a-bbe8-bd9674b2bd17
 
-    rm -rf $TEST_TMPDIR/merkelogs
+    rm -rf $replicadir
     # first get the prod public tenant replicated for massif 0. NOTE: this is a full massif
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --massif 0 --replicadir=$TEST_TMPDIR/merklelogs)
-    assertEquals "watch-public should return a 0 exit code" 0 $?
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --massif 0 --replicadir=$replicadir)
+    assertEquals "should return a 0 exit code" 0 $?
 
-    COUNT=$(find $TEST_TMPDIR/merklelogs -type f | wc -l | tr -d ' ')
+    COUNT=$(find $replicadir -type f | wc -l | tr -d ' ')
     assertEquals "should replicate one massif and one seal" "2" "$COUNT"
 
     # now get a different prod tenant log and seal. NOTE the log is partially full for this tenant
@@ -223,20 +239,74 @@ testReplicateLocalErrorForTamperedFullToPartialMassif() {
     curl -s -H "x-ms-blob-type: BlockBlob" -H "x-ms-version: 2019-12-12" $tampered_seal_url -o tampered.sth
 
     # copy over the different tenant log for massif 0
-    cp tampered.log $TEST_TMPDIR/merklelogs/$PROD_PUBLIC_TENANT_ID/0/massifs/0000000000000000.log
+    cp tampered.log $replicadir/$PROD_PUBLIC_TENANT_ID/0/massifs/0000000000000000.log
 
     # attempt to replicate the logs again, the local log data is for the wrong tenant but the local seal is correct for the replaced data and the remote seal
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --replicadir=$TEST_TMPDIR/merklelogs)
-    assertEquals "1: a tampered log should exit 1" 1 $?
-    assertContains "$output"  "error: consistency check failed: the accumulator produced for the trusted base state doesn't match the root produced for the seal state fetched from the log"
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --replicadir=$replicadir)
+    assertEquals "1: extending an inconsistent replica should exit 1" 1 $?
+    assertContains "$output"  "error: the seal signature verification failed: failed to verify seal for massif 0"
 
     # now add in the seal from the other log, so that the local log and seal are consistent and locally verifiable.
-    cp tampered.sth $TEST_TMPDIR/merklelogs/$PROD_PUBLIC_TENANT_ID/0/massifseals/0000000000000000.sth
+    cp tampered.sth $replicadir/$PROD_PUBLIC_TENANT_ID/0/massifseals/0000000000000000.sth
 
     # attempt to replicate the logs again
-    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --latest --replicadir=$TEST_TMPDIR/merklelogs)
-    assertEquals "2: a tampered log should exit 1" 1 $?
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$PROD_PUBLIC_TENANT_ID replicate-logs --latest --replicadir=$replicadir)
+    assertEquals "2: extending an inconsistent replica should exit 1" 1 $?
     assertContains "$output"  "error: consistency check failed: the accumulator produced for the trusted base state doesn't match the root produced for the seal state fetched from the log"
+}
+
+# test veracity can't update the replica for a tenant whos log has been tampered with
+#
+# This test repeats testReplicateErrorForMixedTenants, but does so using the
+# combination of watch | replicate-logs which permits finer control over the
+# replica
+testReplicateLocalErrorForTamperedPartialToFullMassif() {
+
+    local output
+    local other_tenant
+    local tampered_log_url
+    local tampered_seal_url
+
+    local replicadir=$TEST_TMPDIR/merklelogs
+
+    # Note: this tenant is known to have > 1 massif at the time of writing and logs don't get shorter
+    other_tenant=tenant/b197ba3c-44fe-4b1a-bbe8-bd9674b2bd17
+
+    # first get the prod tenant replicated for massif 0. NOTE: this is a partially full massif
+    rm -rf $replicadir
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata \
+        --tenant=$other_tenant watch --horizon 10000h \
+        | $VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$other_tenant replicate-logs --ancestors=0 --massif 0 --replicadir=$replicadir)
+    assertEquals "watch-public should return a 0 exit code" 0 $?
+
+    COUNT=$(find $replicadir -type f | wc -l | tr -d ' ')
+    assertEquals "should replicate one massif and one seal" "2" "$COUNT"
+
+    # now get a different prod public tenant log and seal. NOTE: this is a full massif
+    tampered_log_url=${tampered_log_url:-${DATATRAILS_URL}/verifiabledata/merklelogs/v1/mmrs/${PROD_PUBLIC_TENANT_ID}/0/massifs/0000000000000000.log}
+    tampered_seal_url=${tampered_seal_url:-${DATATRAILS_URL}/verifiabledata/merklelogs/v1/mmrs/${PROD_PUBLIC_TENANT_ID}/0/massifseals/0000000000000000.sth}
+    curl -s -H "x-ms-blob-type: BlockBlob" -H "x-ms-version: 2019-12-12" $tampered_log_url -o tampered.log
+    curl -s -H "x-ms-blob-type: BlockBlob" -H "x-ms-version: 2019-12-12" $tampered_seal_url -o tampered.sth
+
+    # copy over the different tenant log for massif 0
+    cp tampered.log $replicadir/$other_tenant/0/massifs/0000000000000000.log
+
+    # attempt to replicate the logs again
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata \
+        --tenant=$other_tenant watch --horizon 10000h \
+        | $VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$other_tenant replicate-logs --replicadir=$replicadir)
+    assertEquals "extending an inconsistent replica should exit 1" 1 $?
+    assertContains "$output"  "error: the seal signature verification failed: failed to verify seal for massif 0"
+
+    # now attempt to change the seal to the tampered log seal
+    cp tampered.sth $replicadir/$other_tenant/0/massifseals/0000000000000000.sth
+
+    # attempt to replicate the logs again
+    output=$($VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata \
+        --tenant=$other_tenant watch --horizon 10000h \
+        | $VERACITY_INSTALL --data-url $DATATRAILS_URL/verifiabledata --tenant=$other_tenant replicate-logs --replicadir=$replicadir)
+    assertEquals "extending an inconsistent replica should exit 1" 1 $?
+    assertContains "$output"  "error: consistency check failed"
 }
 
 testVerboseOutput() {

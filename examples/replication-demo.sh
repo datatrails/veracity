1cab940783fa3308babb2e06fd9b9463d36a2a14
Robin Bryce <robin.bryce@datatrails.ai>
2025-03-05 08:22:36 +0100

    review: make the full replica mode explicit
    and remove the ability to replicate and aribtrary set of tenants

--- examples/replication-demo.sh
+++ examples/replication-demo.sh
@@ -14,14 +14,12 @@ DATATRAILS_URL=${DATATRAILS_URL:-https://app.datatrails.ai}
 # For development and testing setting this to "go run cmd/veracity/main.go" is useful.
 VERACITY_BIN=${VERACITY_BIN:-veracity}
 
-# Default to replicating just the public tenant. This will cause the script to
-# report an update for creation of a public event by any tenant.
-# use `-t ""` to replicate all tenants
-REPLICATE_TENANT_OPTION="--tenant tenant/6ea5cd00-c711-3649-6914-7b125928bbb4"
 
-# The tenant to watch for changes. We default to the public tenant so that a
-# public event registered in any tenant will be spotted by this script.
-MONITOR_TENANT="tenant/6ea5cd00-c711-3649-6914-7b125928bbb4"
+FULL_REPLICA=false
+
+# To give a feel for the replication process, we will watch for changes in a
+# single tenant's log. This is not necessary for normal replication.
+MONITOR_CHANGES_FOR_TENANT="tenant/6ea5cd00-c711-3649-6914-7b125928bbb4"
 
 REPLICADIR=merklelogs
 
@@ -35,23 +33,23 @@ usage() {
 
 usage: $SCRIPTNAME 
 
+    -f              set to replicate all tenants
     -d              veracity replicate-logs --replicadir value, default: $REPLICADIR
-    -o              --tenant option, default: $REPLICATE_TENANT_OPTION
     -s              interval between replication attempts in seconds, default $REPLICATION_INTERVAL
-    -t              tenant, tenant to watch for changes default: $MONITOR_TENANT
+    -t              a single tenant to watch for changes, defaults to the public tenant
 END
     exit 1
 }
 
-while getopts "d:o:s:t:" o; do
+while getopts "d:fo:s:t:" o; do
     case "${o}" in
         d)  REPLICADIR=$OPTARG
             ;;
-        o)  REPLICATE_TENANT_OPTION=$OPTARG
+        f)  FULL_REPLICA=true
             ;;
         s)  REPLICATION_INTERVAL=$OPTARG
             ;;
-        t)  MONITOR_TENANT=$OPTARG
+        t)  MONITOR_CHANGES_FOR_TENANT=$OPTARG
             ;;
         *)  usage
             ;;
@@ -63,11 +61,18 @@ shift $((OPTIND-1))
 
 
 run() {
+    local tenants_to_replicate="--tenant tenant/6ea5cd00-c711-3649-6914-7b125928bbb4"
+    if $FULL_REPLICA; then
+        # The default, when no tenants are specified to replicate-logs, is to replicate all tenants
+        tenants_to_replicate=""
+    fi
+
     $VERACITY_BIN --data-url $DATATRAILS_URL/verifiabledata \
-         $REPLICATE_TENANT_OPTION replicate-logs --progress --latest --replicadir=$REPLICADIR
+         $tenants_to_replicate replicate-logs --progress --latest --replicadir=$REPLICADIR
+
 
     # identify the filename of the last massif replicated for the tnenant
-    local last_massif=$(ls $REPLICADIR/$MONITOR_TENANT/0/massifs/*.log | sort -n | tail -n 1)
+    local last_massif=$(ls $REPLICADIR/$MONITOR_CHANGES_FOR_TENANT/0/massifs/*.log | sort -n | tail -n 1)
     echo "last_massif: $last_massif"
 
     # take its hash so we can tell if it changed
@@ -76,18 +81,18 @@ run() {
     while true; do
 
         $VERACITY_BIN --data-url $DATATRAILS_URL/verifiabledata \
-            $REPLICATE_TENANT_OPTION replicate-logs  --progress --latest --replicadir=$REPLICADIR
+            $tenants_to_replicate replicate-logs  --progress --latest --replicadir=$REPLICADIR
 
         # This handles a case that is only significant to the way this script
         # reports. In normal use there is no need to do this.
-        local new_last_massif=$(ls $REPLICADIR/$MONITOR_TENANT/0/massifs/*.log | sort -n | tail -n 1)
+        local new_last_massif=$(ls $REPLICADIR/$MONITOR_CHANGES_FOR_TENANT/0/massifs/*.log | sort -n | tail -n 1)
         if [ "$last_massif" != "$new_last_massif" ]; then
             last_massif=$new_last_massif
         fi
 
         local sum_cur=$($SHASUM_BIN $last_massif | awk '{print $1}')
         if [ "$sum_last" != "$sum_cur" ]; then
-            echo "The log grew for tenant $MONITOR_TENANT, old hash: $sum_last, new hash: $sum_cur"
+            echo "The log grew for tenant $MONITOR_CHANGES_FOR_TENANT, old hash: $sum_last, new hash: $sum_cur"
             sum_last=$sum_cur
         fi
         echo "Sleeping for $REPLICATION_INTERVAL seconds (Use Ctrl-C to exit)"

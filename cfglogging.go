c5f919c5918dc060d630c7133b6f0c5e9b8bcc02
Robin Bryce <robin.bryce@datatrails.ai>
2025-03-19 11:23:19 +0000

    fix: use the TEST logger and explain why in a comment

--- cfglogging.go
+++ cfglogging.go
@@ -18,8 +18,11 @@ func cfgLogging(cmd *CmdCtx, cCtx *cli.Context) error {
 	// instancing the entire veracity command, including its logging
 	// initialisation, in the integration tests. In cases where the test needs
 	// to be threaded, this can cause a data race due to the way our logging
-	// package deals with its global process state.
-	if logLevel == "NOOP" {
+	// package deals with its global process state.  NOTE: Veracity uses the
+	// NOOP logger so that consol output can be produced cleanly via the logging
+	// package. For that reason we override the TEST logger instead. It is only
+	// integration tests that need to make use of this.
+	if logLevel == "TEST" {
 		cmd.log = &logger.WrappedLogger{
 			SugaredLogger: zap.NewNop().Sugar(),
 		}
